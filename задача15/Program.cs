414f807d2c3991f82c08232f197ba7108ba7b95d
Tatiana <dlyarabotochki@mail.ru>
2022-10-07 01:50:50 +0300

    задача15

---
+++ задача15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число от 1-7");
+int num = int.Parse(Console.ReadLine()!);
+if (num==6||num==7)
+{
+    Console.WriteLine("Да,выходной");
+}
+else
+{
+  Console.WriteLine("Нет,работаем");  
+}
